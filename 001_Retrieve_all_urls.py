a189b97384b7ab0f9eb812254b942036a90c513a
Ivan Khomiakov <iskhomiakov@gmail.com>
2023-08-10 17:11:32 +0700

    RER-15

--- 001_Retrieve_all_urls.py
+++ 001_Retrieve_all_urls.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-*** To be able to run this script, selenium and google chrome webdriver are needed.
-
-This script will:
-1. retrieve url links for all district in Bangkok, Thailand listed on hipflat website.
-2. retrieve url links for all condomeniums listed on each district.
-3. save the data to condo_links_all.txt
-"""
 
 # Import packages
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 import time
@@ -18,23 +11,30 @@ import pickle as pk
 
 # Create an 'instance' of the driver.
 # A new Chrome (or other browser) window should open up if options.headless = False (default)
+# Create an 'instance' of the driver.
+# A new Chrome (or other browser) window should open up if options.headless = False (default)
 CHROMEDRIVER_PATH = "./_chromedriver/chromedriver.exe"
 options = Options()
-options.headless = True
-driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
+#options.add_argument('--headless')
+#options.add_argument(CHROMEDRIVER_PATH)
+driver = webdriver.Chrome(options=options)
 
-# Enter the main page, all condos in Bangkok are grouped by district.
-url ='https://www.hipflat.co.th/en/market/condo-bangkok-skik'
+# Enter the main page, all condos in Phuket are grouped by district.
+url ='https://www.hipflat.co.th/en/thailand-projects/condo/phuket-pu'
 driver.get(url)
+#assert "Phuket" in driver.title
 
+# Write function to scrape all links from the webpage.
 # Write function to scrape all links from the webpage.
 def get_all_links(driver):
     links = []
-    elements = driver.find_elements_by_class_name('directories__lists-element-name')
+    elements = driver.find_elements(By.CLASS_NAME, 'directories__lists-element-name')
     for elem in elements:
+        #print(elem.get_attribute("outerHTML"))
         href = elem.get_attribute("href")
         links.append(href)  
     return links
+
 
 # Run and store the links in district_links
 start_time = datetime.now() 
@@ -42,12 +42,11 @@ district_links=get_all_links(driver)
 time_elapsed = datetime.now() - start_time 
 print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
 
-# Check the length of 'district_links', there are 50 districts in Bangkok. 
-# https://en.wikipedia.org/wiki/List_of_districts_of_Bangkok
-print(len(district_links))
+# Check the length of 'district_links', there are 50 districts in Phuket. 
+print('districs number:\t', str(len(district_links)))
 
 # Re-run the function to retrive all condo links in each district.
-# Append to 'condo_links'.
+# Append to condo_links.
 start_time = datetime.now()
 condo_links=[]
 for district in district_links:
@@ -56,6 +55,8 @@ for district in district_links:
 #when searching for an element if it is not immediately present.
     driver.implicitly_wait(10)
     driver.get(district)
+    links = get_all_links(driver)
+    print(links)
     condo_links.append(get_all_links(driver))
     time_elapsed = datetime.now() - start_time 
     print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
@@ -64,6 +65,7 @@ print("completed")
 # Now we got lists within a list (nested list)
 # Turn a (nested) python list into a single list, that contains all the elements of sub lists
 # Named as 'condo_links_all'
+
 from itertools import chain
 condo_links_all=list(chain.from_iterable(condo_links))
 print("Total condo listings = "+str(len(condo_links_all)))
